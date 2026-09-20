fead6864953c3bbd1c23df3f71fc050769cf9eaa
Uliana Kozlova <u.kozlova@g.nsu.ru>
2026-09-20 17:06:38 +0700

    Task 16

---
+++ 25214/u.kozlova/lab16/lab16.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+static int getch(void)
+{
+    struct termios old, new;
+
+	tcgetattr(STDIN_FILENO, &old);
+
+	new = old;
+	new.c_lflag &= ~(ICANON | ECHO);
+
+	tcsetattr(STDIN_FILENO, TCSANOW, &new);
+
+	int c = getchar();
+    if (c == 4) {
+        tcsetattr(STDIN_FILENO, TCSANOW, &old); 
+        printf("\nEOF\n");
+        exit(1);
+    }
+	tcsetattr(STDIN_FILENO, TCSANOW, &old);
+	return c;
+}
+
+int main()
+{
+    printf("Введите символ: ");
+    fflush(stdout);
+
+    int ans = getch();
+
+    printf("\nВаш символ: %c\n", ans);
+
+    return 0;
+}
